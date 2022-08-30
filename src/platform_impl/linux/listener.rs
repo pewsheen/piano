237ba7bcae52f487e2c7139e1e3353493adf743c
Jason Tsai <jason@pews.dev>
2022-08-30 10:26:21 +0800

    chore: organize

--- src/platform_impl/linux/listener.rs
+++ src/platform_impl/linux/listener.rs
@@ -1,5 +1,31 @@
 use std::ptr;
-use x11_dl::xlib;
+use x11_dl::xlib::{self, Xlib, _XDisplay};
+
+fn grab_key(xlib: &Xlib, display: *mut _XDisplay, window: u64) {
+  unsafe {
+    (xlib.XGrabKey)(
+      display,
+      xlib::AnyKey,
+      xlib::AnyModifier,
+      window,
+      1,
+      xlib::GrabModeAsync,
+      xlib::GrabModeAsync,
+    );
+  }
+}
+
+fn ungrab_key(xlib: &Xlib, display: *mut _XDisplay, window: u64) {
+  unsafe {
+    (xlib.XUngrabKey)(display, xlib::AnyKey, xlib::AnyModifier, window);
+  }
+}
+
+fn send_event(xlib: &Xlib, display: *mut _XDisplay, event: *mut xlib::XEvent) {
+  unsafe {
+    (xlib.XSendEvent)(display, xlib::InputFocus as u64, 1, (*event).get_type() as i64, event);
+  }
+}
 
 pub fn run() {
   std::thread::spawn(move || {
@@ -10,30 +36,22 @@ pub fn run() {
       let mut cur_window: u64 = std::mem::MaybeUninit::uninit().assume_init();
       let mut revert: i32 = std::mem::MaybeUninit::uninit().assume_init();
 
-      (xlib.XGetInputFocus)(display, &mut cur_window, &mut revert);
-
       // Only trigger key release at end of repeated keys
       #[allow(clippy::uninit_assumed_init)]
       let mut supported_rtrn: i32 = std::mem::MaybeUninit::uninit().assume_init();
       (xlib.XkbSetDetectableAutoRepeat)(display, 1, &mut supported_rtrn);
 
+      #[allow(clippy::uninit_assumed_init)]
+      let mut event: xlib::XEvent = std::mem::MaybeUninit::uninit().assume_init();
+
+      (xlib.XGetInputFocus)(display, &mut cur_window, &mut revert);
       (xlib.XSelectInput)(
         display,
         cur_window,
         xlib::KeyPressMask | xlib::KeyReleaseMask | xlib::FocusChangeMask,
       );
-      #[allow(clippy::uninit_assumed_init)]
-      let mut event: xlib::XEvent = std::mem::MaybeUninit::uninit().assume_init();
 
-      (xlib.XGrabKey)(
-        display,
-        xlib::AnyKey,
-        xlib::AnyModifier,
-        cur_window,
-        1,
-        xlib::GrabModeAsync,
-        xlib::GrabModeAsync,
-      );
+      grab_key(&xlib, display, cur_window);
 
       loop {
         if (xlib.XPending)(display) > 0 {
@@ -42,28 +60,24 @@ pub fn run() {
             let keycode = event.key.keycode;
             let modifiers = event.key.state;
             println!("keycode: {:?}, modifier: {:?}", keycode, modifiers);
-            // (xlib.XSendEvent)(display, xlib::InputFocus as u64, 1, event.get_type() as i64, &mut event);
           } else if let xlib::FocusOut = event.get_type() {
-            println!("cur_window: {:?}, root: {:?}", cur_window, root);
+            println!("prev: {:?}", cur_window);
+
+            // unregister focus out window's event mask.
             if cur_window != root {
               (xlib.XSelectInput)(display, cur_window, 0);
-              (xlib.XUngrabKey)(display, xlib::AnyKey, xlib::AnyModifier, cur_window);
+              ungrab_key(&xlib, display, cur_window);
             }
 
+            // get current focused window.
             (xlib.XGetInputFocus)(display, &mut cur_window, &mut revert);
             if cur_window == xlib::PointerRoot as u64 {
               cur_window = root;
             }
+            println!("curr: {:?}", cur_window);
 
-            (xlib.XGrabKey)(
-              display,
-              xlib::AnyKey,
-              xlib::AnyModifier,
-              cur_window,
-              1,
-              xlib::GrabModeAsync,
-              xlib::GrabModeAsync,
-            );
+            // register current focused window's event mask.
+            grab_key(&xlib, display, cur_window);
             (xlib.XSelectInput)(
               display,
               root,
